829e4e562d356bf985edc95471c783d3ed415b49
choewy <choewy32@gmail.com>
2022-07-09 16:25:01 +0900

    [init] MySQL8.0 docker-compose

---
+++ database/README.md
@@ -0,0 +1,9 @@
+# Database
+
+- Docker(compose)
+
+## Environment
+
+```env
+MYSQL_ROOT_PASSWORD=${MYSQL_PASSWORD}
+```
---
+++ database/sqls/initialize.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS service;
+
+SET GLOBAL time_zone="Asia/Seoul";
+SET time_zone="Asia/Seoul";
