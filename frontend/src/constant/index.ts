69a8e33a3bc8f62f72616fd3486a5de694431d9b
choewy <choewy32@gmail.com>
2022-07-10 17:02:24 +0900

    [feat] global variables

---
+++ frontend/src/constant/index.ts
@@ -0,0 +1,13 @@
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export const PAGE_PATH = {
+  HOME: '/',
+  KAKAO_LOGIN: '/login/kakao',
+  KAKAO_REDIRECT: '/login/kakao/redirect',
+};
+
+export const PAGE_TITLE = {
+  HOME: '홈',
+  KAKAO_LOGIN: '로그인',
+  KAKAO_REDIRECT: '',
+};
