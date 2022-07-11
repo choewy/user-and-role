b3ff1d5ca3b9c57a35bd009ec27fcdf958ea1ad0
choewy <choewy32@gmail.com>
2022-07-11 16:08:07 +0900

    [edit] global constant variables

--- frontend/src/constant/index.ts
+++ frontend/src/constant/index.ts
@@ -1,4 +1,12 @@
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const SITE_URL = process.env.REACT_APP_SITE_URL;
+
+export const APP_TITLE = 'UNDEFINED';
+
+export const MIN_WIDTH = '375px';
+export const MAX_WIDTH = '812px';
+export const HEADER_HEIGHT = '80px';
+export const NAVBAR_HEIGHT = '80px';
 
 export const PAGE_PATH = {
   HOME: '/',
@@ -6,8 +14,7 @@ export const PAGE_PATH = {
   KAKAO_REDIRECT: '/login/kakao/redirect',
 };
 
-export const PAGE_TITLE = {
-  HOME: '홈',
-  KAKAO_LOGIN: '로그인',
-  KAKAO_REDIRECT: '',
+export const LINKS = {
+  github: 'https://github.com/choewy/undefined',
+  figma: 'https://www.figma.com/file/60Zy3CsLWa9MdoAAxW6LiM/모바일-옷장',
 };
