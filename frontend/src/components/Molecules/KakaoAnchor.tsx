dd7a3862f48b21602f85c0cd438b277d6799a456
choewy <choewy32@gmail.com>
2022-07-11 17:10:54 +0900

    [feat] kakao login anchor style

--- frontend/src/components/Molecules/KakaoAnchor.tsx
+++ frontend/src/components/Molecules/KakaoAnchor.tsx
@@ -2,7 +2,32 @@ import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { kakaoLoginUrlAPI } from '../../api';
 
-const Anchor = styled.a``;
+const Wrapper = styled.div`
+  width: 100%;
+  min-witdh: 200px;
+  max-width: 200px;
+  background: rgba(249, 224, 0, 1);
+  border-radius: 2rem;
+`;
+
+const Hover = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  border-radius: 2rem;
+  &:hover {
+    background: rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const Anchor = styled.a`
+  color: rgb(50, 50, 50);
+  text-decoration: none;
+`;
 
 const KakaoLoginAnchor: React.FC = () => {
   const effectRef = useRef(false);
@@ -28,7 +53,13 @@ const KakaoLoginAnchor: React.FC = () => {
     }
   }, []);
 
-  return <Anchor href={href}>카카오 로그인</Anchor>;
+  return (
+    <Wrapper>
+      <Hover>
+        <Anchor href={href}>카카오 로그인</Anchor>
+      </Hover>
+    </Wrapper>
+  );
 };
 
 export default KakaoLoginAnchor;
