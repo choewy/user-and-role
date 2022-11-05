71f61fc5fb6c87257c0ec306180e22e94f63cf60
choewy <choewy32@gmail.com>
2022-11-05 21:35:01 +0900

    feature/create-user

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -1,13 +1,17 @@
-import { User } from '@/entities';
+import { PolicyKey, Role, User, UserAndRoles } from '@/entities';
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 
 @Injectable()
 export class AuthRepository {
   private readonly userRepository: Repository<User>;
+  private readonly roleRepository: Repository<Role>;
+  private readonly userAndRolesRepository: Repository<UserAndRoles>;
 
   constructor(private readonly dataSource: DataSource) {
     this.userRepository = this.dataSource.getRepository(User);
+    this.roleRepository = this.dataSource.getRepository(Role);
+    this.userAndRolesRepository = this.dataSource.getRepository(UserAndRoles);
   }
 
   async findUserById(id: number) {
@@ -39,7 +43,28 @@ export class AuthRepository {
   }
 
   async insertUser(user: Partial<User>): Promise<number> {
-    const { identifiers } = await this.userRepository.insert(user);
-    return identifiers[0].id;
+    let id: number;
+
+    await this.dataSource.transaction(async () => {
+      const role = await this.roleRepository.findOne({
+        relations: { policies: true },
+        where: {
+          policies: {
+            policyKey: PolicyKey.Empty,
+            isApply: true,
+          },
+        },
+      });
+
+      const { identifiers } = await this.userRepository.insert(user);
+      id = identifiers[0].id;
+
+      await this.userAndRolesRepository.insert({
+        userId: id,
+        roleId: role.id,
+      });
+    });
+
+    return id;
   }
 }
